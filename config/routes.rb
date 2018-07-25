bdaccfd7f1b4311528fa14e88cb29d7685bc2fa7
Rayan Castro <rayancdc@gmail.com>
2018-07-25 17:03:41 -0300

    add review and restaurant controller with working index,show,new and create actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :restaurants do
-
-
-
-    resources :reviews
+    resources :reviews, only: [ :index, :new, :create ]
   end
+  resources :reviews, only: [ :show]
 end
